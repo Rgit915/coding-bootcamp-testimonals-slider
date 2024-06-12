9b7d6929f474bb395802acc2e7c275d86f03cc6b
Rgit915 <rora.goitom@gmail.com>
2024-06-12 15:47:58 -0400

    Extend Tailwind CSS configuration with custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "dark-blue": "hsl(240, 38%, 20%)",
+        "grayish-blue": "hsl(240, 18%, 77%)"
+      },
+      fontFamily:{
+        "inter": ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
